f5ad908b94468ca733a1f18f2d56efac9a2ab43e
Angelo Nazzaro <mutenssj@gmail.com>
2024-06-10 12:53:57 +0200

    implemented pagination logic in backend

--- backend/api/movies.py
+++ backend/api/movies.py
@@ -1,23 +1,52 @@
+import os
 from flask import Blueprint, request, jsonify
-from db import get_movies, get_movies_by_genres, get_movies_by_release_year
+from db import get_movies, get_movies_by_genres, get_movies_by_release_year, sort_movies
 
 from flask_cors import CORS
-from datetime import datetime
 
 
 movies_api = Blueprint(
     'movies_api', 'movies_api', url_prefix='/api/movies')
 
 CORS(movies_api)
+DEFAULT_MOVIES_PER_PAGE = os.getenv('DEFAULT_MOVIES_PER_PAGE')
+DEFAULT_MOVIES_PER_PAGE = int(DEFAULT_MOVIES_PER_PAGE) if DEFAULT_MOVIES_PER_PAGE else 20
 
-@movies_api.route('/', methods=['GET'])
-def api_get_movies():
-    MOVIES_PER_PAGE = 20
-    
-    movies = get_movies()
+
+def paginate_movies(get_movies_func, **kwargs):
+    try:
+        page = int(request.args.get('page', 0))
+    except (TypeError, ValueError) as e:
+        print('Got bad value:', e)
+        page = 0
+
+    offset = page * DEFAULT_MOVIES_PER_PAGE
+    try:
+        movies, total_results = get_movies_func(offset=offset, movies_per_page=DEFAULT_MOVIES_PER_PAGE, **kwargs)
+    except TypeError as e:
+        print('Got bad value:', e)
+        return {}
 
     response = {
         "movies": movies,
+        "page": page,
+        "entries_per_page": DEFAULT_MOVIES_PER_PAGE,
+        "total_results": total_results
     }
 
     return jsonify(response)
+
+
+@movies_api.route('/', methods=['GET'])
+def api_get_movies():
+    return paginate_movies(get_movies)
+
+
+@movies_api.route('/release_year/<int:year>', methods=['GET'])
+def api_get_movies_by_release_year(year):
+    return paginate_movies(get_movies_by_release_year, release_year=year)
+
+
+@movies_api.route('/sort/<field>/<int:order>', methods=['GET'])
+def api_sort_movies(field, order):
+    return paginate_movies(sort_movies, field=field, order=order)
--- backend/db.py
+++ backend/db.py
@@ -20,27 +20,31 @@ from typing import Union, Optional
 import os
 
 from pymongo import MongoClient
-from dotenv import load_dotenv, dotenv_values 
+from dotenv import load_dotenv, dotenv_values
+
+from typing import Optional, Union, List, Tuple
+from pymongo.collection import Collection
+
 # loading variables from .env file
-load_dotenv() 
-    
+load_dotenv()
+
 
 def get_db_name():
     return os.getenv("DB_NAME")
-    
-    
+
+
 def get_db_user():
     return os.getenv("DB_USER")
-    
-    
+
+
 def get_db_password():
     return os.getenv("DB_PASSWORD")
-    
-    
+
+
 def get_db_host():
     return os.getenv("DB_HOST")
-    
-    
+
+
 def get_db_port():
     return os.getenv("DB_PORT")
 
@@ -51,14 +55,14 @@ def get_connection_string():
     db_host = get_db_host()
     db_port = get_db_port()
     db_name = get_db_name()
-    
+
     return f"mongodb://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?retryWrites=true&w=majority"
 
 
 def open_connection():
-    return MongoClient(get_connection_string()) 
-    
-    
+    return MongoClient(get_connection_string())
+
+
 def close_connection(client):
     client.close()
 
@@ -72,12 +76,12 @@ def get_db():
     if db is None:
         client = open_connection()
         db_name = get_db_name()
-    
+
         if db_name is None:
             raise ValueError("DB_NAME is not set in .env file")
-    
+
         db = g._database = getattr(client, db_name)
-       
+
     return db
 
 
@@ -85,52 +89,158 @@ def get_db():
 db = LocalProxy(get_db)
 
 
-def get_movies(title: Optional[str] = None, directors: Optional[Union[str, list[str]]] = None, 
-    actors: Optional[Union[str, list[str]]] = None):
-    
-    if directors and isinstance(directors, str):
-        directors = [directors]
-        
-    if actors and isinstance(actors, str):
-        actors = [actors]
-    
-    query = {}
-    
-    if title:
-        query["title"] = title
-    
-    if directors:
-        query["directors"] = {"$in": directors}
-    
-    if actors:
-        query["cast"] = {"$in": actors}
-    
+def paginate_query(collection, query, offset: int, limit: int):
+    """
+    Paginate a MongoDB query.
+
+    :param collection: The collection from which you want to paginate.
+    :param query: The MongoDB query to paginate.
+    :param offset: The number of documents to skip.
+    :param limit: The maximum number of documents to return.
+    :return: A tuple containing the paginated results and the total count of documents.
+    """
+    results = query.skip(offset).limit(limit)
+    total_count = collection.count_documents({})
+    return list(results), total_count
+
+
+def project_movies(query, projection):
+    """
+    Project fields for a MongoDB query.
+
+    :param query: The MongoDB query to project fields from.
+    :param projection: The fields to include or exclude.
+    :return: A MongoDB query with projection applied.
+    """
+    return query.project(projection)
+
+
+def get_movies(offset: int, movies_per_page: int, title: Optional[str] = None,
+               directors: Optional[Union[str, List[str]]] = None,
+               actors: Optional[Union[str, List[str]]] = None, projection: Optional[dict] = None) \
+        -> Union[Tuple[List[dict], int], Exception]:
+    """
+    Get movies based on title, directors, and actors.
+
+    :param offset: The number of documents to skip.
+    :param movies_per_page: The maximum number of movies to return per page.
+    :param title: The title of the movie.
+    :param directors: Directors of the movie.
+    :param actors: Actors starring in the movie.
+    :param projection: The fields to include or exclude in the result.
+    :return: A tuple containing the list of movies for the given page and the total count of movies that match
+        the query criteria or an Exception if an error occurs.
+    """
+
     try:
-        return list(db.movie.find(query))
+        query = {}
+
+        if title:
+            query["title"] = title
+
+        if directors:
+            if isinstance(directors, str):
+                directors = [directors]
+            query["directors"] = {"$in": directors}
+
+        if actors:
+            if isinstance(actors, str):
+                actors = [actors]
+            query["cast"] = {"$in": actors}
+
+        # Default projection if not provided
+        default_projection = {"_id": 1, "title": 1, "poster": 1, "release_year": 1, "popularity": 1, "vote_average": 1}
+        projection = projection or default_projection
+
+        # Construct the MongoDB query with projection
+        movie_query = db.movie.find(query, projection)
+
+        # Paginate the query
+        movies, total_movies = paginate_query(db.movie, movie_query, offset, movies_per_page)
+
+        return movies, total_movies
     except Exception as e:
         return e
-        
 
-def get_movies_by_genres(genres: Union[list[str], str]):
+
+def get_movies_by_genres(offset: int, movies_per_page: int, genres: Union[List[str], str],
+                         projection: Optional[dict] = None) -> Union[Tuple[List[dict], int], Exception]:
     """
-    Finds and returns movies by genres.
+    Get movies based on genres with pagination and projection.
+
+    :param projection: The fields to include or exclude in the result.
+    :param offset: The number of documents to skip.
+    :param movies_per_page: The maximum number of movies to return per page.
+    :param genres: List of genres or a single genre.
+    :return: A tuple containing the list of movies for the given page and the total count of movies that match the criteria,
+             or an Exception if an error occurs.
     """
-    
-    if isinstance(genres, str):
-        genres = [genres]
-        
     try:
-        return list(db.movie.find({"genres": {"$all": genres}}))
+        query = {"genres": {"$all": genres} if isinstance(genres, list) else genres}
+
+        # Default projection if not provided
+        default_projection = {"_id": 1, "title": 1, "poster": 1, "release_year": 1, "popularity": 1, "vote_average": 1}
+        projection = projection or default_projection
+
+        # Construct the MongoDB query with projection
+        movie_query = db.movie.find(query, projection)
+
+        # Paginate the query
+        return paginate_query(db.movie, movie_query, offset, movies_per_page)
     except Exception as e:
         return e
 
 
-def get_movies_by_release_year(release_year: int):
+def get_movies_by_release_year(offset: int, movies_per_page: int, release_year: int,
+                               projection: Optional[dict] = None) -> Union[Tuple[List[dict], int], Exception]:
     """
-    Finds and returns movies by release year.
+    Get movies released in a specific year with pagination and projection.
+
+    :param projection: The fields to include or exclude in the result.
+    :param offset: The number of documents to skip.
+    :param movies_per_page: The maximum number of movies to return per page.
+    :param release_year: The release year.
+    :return: A tuple containing the list of movies for the given page and the total count of movies that match
+        the criteria or an Exception if an error occurs.
     """
-        
     try:
-        return list(db.movie.find({"release_year": release_year}))
+        query = {"release_year": release_year}
+
+        # Default projection if not provided
+        default_projection = {"_id": 1, "title": 1, "poster": 1, "release_year": 1, "popularity": 1, "vote_average": 1}
+        projection = projection or default_projection
+
+        # Construct the MongoDB query with projection
+        movie_query = db.movie.find(query, projection)
+
+        # Paginate the query
+        return paginate_query(db.movie, movie_query, offset, movies_per_page)
+    except Exception as e:
+        return e
+
+
+def sort_movies(offset: int, movies_per_page: int, field: str, order: int = -1,
+                projection: Optional[dict] = None) -> Union[Tuple[List[dict], int], Exception]:
+    """
+    Get movies sorted in descending order by rating.
+
+    :param order: The order to sort by.
+    :param field: The field to sort by.
+    :param projection: The fields to include or exclude in the result.
+    :param offset: The number of documents to skip.
+    :param movies_per_page: The maximum number of movies to return per page.
+    :return: A tuple containing the list of movies for the given page and the total count of movies that match
+        the criteria or an Exception if an error occurs.
+    """
+    try:
+        # Default projection if not provided
+        default_projection = {"_id": 1, "title": 1, "poster": 1, "release_year": 1, "popularity": 1, "vote_average": 1}
+        projection = projection or default_projection
+
+        # Construct the MongoDB query with projection
+        movie_query = db.movie.find({}, projection).sort({field: order})
+
+        # Paginate the query
+        return paginate_query(db.movie, movie_query, offset, movies_per_page)
     except Exception as e:
         return e
